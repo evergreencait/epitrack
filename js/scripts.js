7b942c9d6784dfaa8e7c93a8c98c1fcb5aec315b
Caitlin Hines <caitlinhines@me.com>
2017-01-13 11:33:21 -0800

    add conditions to branching for city and style

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ var time = parseInt($("input#time").val());
 var style = $("input:checked").val();
 var learn = $("input:checked").val();
 var company = $("input:checked").val();
+var city = $("input#city").val();
 
 
 $(document).ready(function() {
@@ -10,13 +11,13 @@ $(document).ready(function() {
 
       if (time <= 1) {
         alert("Sorry, you don't have enough time for any tracks");
-      } else if (time > 1 && style === "back-end" && company === "Small start-up") {
+      } else if (time > 1 && style === "back-end" && company === "Small start-up" && style === "Javascript" || "CSS" && city === Seattle || Portland)) {
           $(".ruby").show();
-      } else if (time > 1 && style === "back-end" && company === "Large company"){
+      } else if (time > 1 && style === "back-end" && company === "Large company" && style === "Javascript" || "CSS" && city === Seattle || Portland)){
           $(".php").show();
-      } else if (time > 1 && style === "front-end" && company ==="Small start-up"){
+      } else if (time > 1 && style === "front-end" && company ==="Small start-up" && style === "Javascript" || "CSS" && city === Seattle || Portland)){
           $(".css").show();
-      }  else if (time > 1 && style === "front-end" && company === "Large company"){
+      }  else if (time > 1 && style === "front-end" && company === "Large company" && style === "Javascript" || "CSS" && city === Seattle || Portland){
           $(".design").show();
       } else {
         alert("sorry, we need more information to make a recommendation")
