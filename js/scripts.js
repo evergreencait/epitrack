d7f69d33918853083e601e0e55f3940391d83a64
Caitlin Hines <caitlinhines@me.com>
2017-01-13 15:12:46 -0800

    change branching try and solve an issu

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,37 @@
-var userTime = parseInt($("input#time").val());
-var userStyle = $("input:radio[name=style]:checked").val();
-var userLearn = $("input:radio[name=learn]:checked").val();
-var userCompany = $("input:radio[name=company]:checked").val();
-var userCity = $("#city").val();
+var time = parseInt($("input#time").val());
+var style = $("input:radio[name=style]:checked").val();
+var learn = $("input:radio[name=learn]:checked").val();
+var company = $("input:radio[name=company]:checked").val();
+var city = $("#city").val();
 
 
 $(document).ready(function() {
   $("#questions form").submit(function(event){
     event.preventDefault();
 
-      if (userTime <= 1) {
+      if (time <= 1) {
         alert("Sorry, you don't have enough time to do any tracks")
       } else {
-
-         if (userStyle === "Back-end" && userCompany === "Start-up" && userStyle === "Javascript" || "CSS" && city === "Seattle" || "Portland"){
+      if (city === "Seattle") {
+         if (style === "Back-end" && company === "Start-up" && style === "Javascript" || "CSS"){
           $(".ruby").show();
-        } else if (userStyle === "back-end" && userCompany === "Large company"  && userStyle === "Javascript" || "CSS" && userCity === "Seattle" || "Portland"){
+        } else if (style === "back-end" && company === "Large company"  && style === "Javascript" || "CSS"){
           $(".php").show();
-        } else if (userStyle === "front-end" && userCompany ==="Small start-up"  && userStyle === "Javascript" || "CSS" && userCity === "Seattle" || "Portland"){
+        } else if (style === "front-end" && company ==="Small start-up"  && style === "Javascript" || "CSS"){
           $(".css").show();
-        }  else if (userStyle === "front-end" && userCompany === "Large company"  && userStyle === "Javascript" || "CSS" && userCity === "Seattle" || "Portland"){
+        }  else if (style === "front-end" && company === "Large company"  && style === "Javascript" || "CSS"){
           $(".design").show();
-        } else {
+        }
+      } else if (city === "Portland")
+      if (style === "Back-end" && company === "Start-up" && style === "Javascript" || "CSS"){
+       $(".ruby").show();
+     } else if (style === "back-end" && company === "Large company"  && style === "Javascript" || "CSS"){
+       $(".php").show();
+     } else if (style === "front-end" && company ==="Small start-up"  && style === "Javascript" || "CSS"){
+       $(".css").show();
+     }  else if (style === "front-end" && company === "Large company"  && style === "Javascript" || "CSS"){
+       $(".design").show();
+     } else
         alert("Sorry! We need more information to make a recommendation")
         }
       }
