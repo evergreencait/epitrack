ad850a84e905992976c14eea8e8c515d446ef950
Caitlin Hines <caitlinhines@me.com>
2017-01-13 10:04:38 -0800

    complete variable definitions

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,18 @@
-var city =
-var time =
-var style =
-var learn = 
-
+var city = $(#city).val();
+var time = parseInt($(#time).val());
+var style = $("input:checked").val();
+var learn = $("input:checked").val();
+var company = $("input:checked").val();
 
 
 $(document).ready(function(){
+  $("questions").submit(event){
+    event.preventDefault();
+
+  if (time &&  )
 
 
 
+
+  }
 });
