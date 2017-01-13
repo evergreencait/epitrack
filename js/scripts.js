90cd2d4e6a253cb626304fd46dee85fa039225d5
Caitlin Hines <caitlinhines@me.com>
2017-01-13 11:42:22 -0800

    fix error in syntax within branching and makes branching functional

--- js/scripts.js
+++ js/scripts.js
@@ -11,16 +11,17 @@ $(document).ready(function() {
 
       if (time <= 1) {
         alert("Sorry, you don't have enough time for any tracks");
-      } else if (time > 1 && style === "back-end" && company === "Small start-up" && style === "Javascript" || "CSS" && city === Seattle || Portland)) {
+      } else if (time > 1 && style === "back-end" && company === "Small start-up" && style === "Javascript" || "CSS" && city === "Seattle" || "Portland"){
           $(".ruby").show();
-      } else if (time > 1 && style === "back-end" && company === "Large company" && style === "Javascript" || "CSS" && city === Seattle || Portland)){
+      } else if (time > 1 && style === "back-end" && company === "Large company"  && style === "Javascript" || "CSS" && city === "Seattle" || "Portland"){
           $(".php").show();
-      } else if (time > 1 && style === "front-end" && company ==="Small start-up" && style === "Javascript" || "CSS" && city === Seattle || Portland)){
+      } else if (time > 1 && style === "front-end" && company ==="Small start-up"  && style === "Javascript" || "CSS" && city === "Seattle" || "Portland"){
           $(".css").show();
-      }  else if (time > 1 && style === "front-end" && company === "Large company" && style === "Javascript" || "CSS" && city === Seattle || Portland){
+      }  else if (time > 1 && style === "front-end" && company === "Large company"  && style === "Javascript" || "CSS" && city === "Seattle" || "Portland"){
           $(".design").show();
       } else {
-        alert("sorry, we need more information to make a recommendation")
+        alert("Sorry! We need more information to make a recommendation")
       }
+
     });
 });
