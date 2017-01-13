352c94549831683479fc9d442416e7ac81df10e3
Caitlin Hines <caitlinhines@me.com>
2017-01-13 14:51:25 -0800

    add README

---
+++ README.md
@@ -0,0 +1,25 @@
+# Language Track Suggester
+
+#### JQuery and Javascript Peer Review for Epicodus, 01.13.2017
+
+#### By Caitlin Hines
+
+## Description
+
+This website allows you to answer questions about yourself to determine what track is best for you at Epicodus. Open the webpage and answer the questions and the site will generate a suggestion.
+
+## Setup/Installation Requirements
+
+Clone this repository
+Open index.html in web browser
+
+
+## Support and contact details
+
+Contact: caitlinhines@me.com
+
+### License
+
+
+Copyright (c) 2016 Caitlin Hines
+This software is licensed under the MIT license.
--- js/scripts.js
+++ js/scripts.js
@@ -10,18 +10,19 @@ $(document).ready(function() {
     event.preventDefault();
 
       if (userTime <= 1) {
-        alert("Sorry, you don't have enough time for any tracks");
-      } else if (userTime > 1 && userStyle === "Back-end" && userCompany === "Start-up" && userStyle === "Javascript" || "CSS" && city === "Seattle" || "Portland"){
-          $(".ruby").show();
-      } else if (userTime > 1 && userStyle === "back-end" && userCompany === "Large company"  && userStyle === "Javascript" || "CSS" && userCity === "Seattle" || "Portland"){
-          $(".php").show();
-      } else if (userTime > 1 && userStyle === "front-end" && userCompany ==="Small start-up"  && userStyle === "Javascript" || "CSS" && userCity === "Seattle" || "Portland"){
-          $(".css").show();
-      }  else if (userTime > 1 && userStyle === "front-end" && userCompany === "Large company"  && userStyle === "Javascript" || "CSS" && userCity === "Seattle" || "Portland"){
-          $(".design").show();
+        alert("Sorry, you don't have enough time to do any tracks")
       } else {
-        alert("Sorry! We need more information to make a recommendation")
-      }
 
-    });
+         if (userStyle === "Back-end" && userCompany === "Start-up" && userStyle === "Javascript" || "CSS" && city === "Seattle" || "Portland"){
+          $(".ruby").show();
+        } else if (userStyle === "back-end" && userCompany === "Large company"  && userStyle === "Javascript" || "CSS" && userCity === "Seattle" || "Portland"){
+          $(".php").show();
+        } else if (userStyle === "front-end" && userCompany ==="Small start-up"  && userStyle === "Javascript" || "CSS" && userCity === "Seattle" || "Portland"){
+          $(".css").show();
+        }  else if (userStyle === "front-end" && userCompany === "Large company"  && userStyle === "Javascript" || "CSS" && userCity === "Seattle" || "Portland"){
+          $(".design").show();
+        } else {
+        alert("Sorry! We need more information to make a recommendation")
+        }
+      }
 });
