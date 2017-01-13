d76e952b89f0c66da78cae0c8b411b8ced683600
Caitlin Hines <caitlinhines@me.com>
2017-01-13 11:17:00 -0800

    fix { errors in branching in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,25 @@
-var city = $(#city).val();
-var time = parseInt($(#time).val());
+var time = parseInt($("input#time").val());
 var style = $("input:checked").val();
 var learn = $("input:checked").val();
 var company = $("input:checked").val();
 
 
-$(document).ready(function(){
-  $("form#questions").submit(event){
+$(document).ready(function() {
+  $("#questions form").submit(function(event){
     event.preventDefault();
 
-  if (time <= 1) {
-    alert("Sorry, you don't have enough time for any tracks")
-  } else if {
-    (time > 1 && style === "back-end" && company === "Small start-up") {
-      $(".ruby").show();
-  } else if {
-    (time > 1 && style === "back-end" && company === "Large company")
-      $(".php").show();
-  } else if {
-    (time > 1 && style === "front-end" && company ==="Small start-up")
-      $(".css").show();
-  }  else if {
-    (time > 1 && style === "front-end" && company ==== "Large company")
-      $(".design").show();
-  } else {
-    alert("sorry, we need more information to make a recommendation")
-  }
+      if (time <= 1) {
+        alert("Sorry, you don't have enough time for any tracks");
+      } else if (time > 1 && style === "back-end" && company === "Small start-up") {
+          $(".ruby").show();
+      } else if (time > 1 && style === "back-end" && company === "Large company"){
+          $(".php").show();
+      } else if (time > 1 && style === "front-end" && company ==="Small start-up"){
+          $(".css").show();
+      }  else if (time > 1 && style === "front-end" && company === "Large company"){
+          $(".design").show();
+      } else {
+        alert("sorry, we need more information to make a recommendation")
+      }
+    });
 });
