d8b79ba7cd8a62ea505a50c44c5a54ce4fca5180
Caitlin Hines <caitlinhines@me.com>
2017-01-15 08:49:07 -0800

    change buttons to radio and get it to working state with branching

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,52 @@
-var time = parseInt($("input#time").val());
-var style = $("input:radio[name=style]:checked").val();
-var learn = $("input:radio[name=learn]:checked").val();
-var company = $("input:radio[name=company]:checked").val();
-var city = $("input#city").val();
+$(document).ready(function(){
+  $("form#questions").submit(function(event){
+
+    $(".ruby").hide();
+    $(".php").hide();
+    $(".css").hide();
+    $(".design").hide();
 
 
+    var styleInput = $("input[name=style]:checked").val();
+    var companyInput = $("input[name=company]:checked").val();
+    var cityInput = $("input[name=city]:checked").val();
+    var timeInput = $("#time").val();
+    var personInput = $("input#person").val()
 
-$(document).ready(function() {
-  $("form#question").submit(function(event){
 
-
-      if (time < 1) {
-        alert("Sorry, you don't have enough time to do any tracks")
-      } else {
-      if (city === "Seattle") {
-         if (style === "Back-end" && company === "Start-up"){
-          $(".ruby").show();
-        } else if (style === "back-end" && company === "Large company"){
-          $(".php").show();
-        } else if (style === "front-end" && company ==="Small start-up"){
-          $(".css").show();
-        }  else if (style === "front-end" && company === "Large company"){
-          $(".design").show();
-        }
-    } else if (city === "Portland") {
-      if (style === "Back-end" && company === "Start-up"){
-        $(".ruby").show();
-      } else if (style === "back-end" && company === "Large company"){
-       $(".php").show();
-      } else if (style === "front-end" && company ==="Small start-up"){
-       $(".css").show();
-      }  else if (style === "front-end" && company === "Large company"){
-       $(".design").show();
-      } else
-          alert("Sorry! We need more information to make a recommendation")
-      }
-    }
-      event.preventDefault();
-    });
-  });
+      if (timeInput === "0-1 month") {
+             alert("Sorry, you don't have time to do a track!");
+           } else {
+             if (cityInput === "portland") {
+               if (companyInput === "start-up" && styleInput === "back-end") {
+                 $(".person").text(personInput);
+                 $(".ruby").show();
+               } else if (companyInput === "start-up" && styleInput === "front-end") {
+                 $(".person").text(personInput);
+                 $(".design").show();
+               } else if (companyInput === "large" && styleInput === "back-end") {
+                  $(".person").text(personInput);
+                 $(".php").show();
+               } else if (companyInput === "large" && styleInput === "front-end") {
+                 $(".person").text(personInput);
+                 $(".css").show();
+               }
+             } else if (cityInput === "seattle") {
+               if (companyInput === "start-up" && styleInput === "back-end") {
+                 $(".person").text(personInput);
+                 $(".ruby").show();
+               } else if (companyInput === "start-up" && styleInput === "front-end") {
+                 $(".person").text(personInput);
+                 $(".design").show();
+               } else if (companyInput === "large" && styleInput === "back-end") {
+                 $(".person").text(personInput);
+                 $(".php").show();
+               } else if (companyInput === "large" && styleInput === "front-end") {
+                 $(".person").text(personInput);
+                 $(".css").show();
+               }
+             }
+           }
+           event.preventDefault();
+         });
+       });
